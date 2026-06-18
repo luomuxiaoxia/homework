5c7314ee4ffacd15014178005cd302bedadf322c
luomuxiaoxia <luomuxiaoxia173@gmail>
2026-06-18 13:58:30 +0800

    ex02: 初始版本，实现1Hz非阻塞闪烁

---
+++ ex02/ex02.ino
@@ -0,0 +1,19 @@
+const int ledPin = 2;
+unsigned long previousMillis = 0;
+const long interval = 1000;   // 1秒
+int ledState = LOW;
+
+void setup() {
+  pinMode(ledPin, OUTPUT);
+  Serial.begin(115200);
+}
+
+void loop() {
+  unsigned long currentMillis = millis();
+  if (currentMillis - previousMillis >= interval) {
+    previousMillis = currentMillis;
+    ledState = !ledState;          // 翻转状态
+    digitalWrite(ledPin, ledState);
+    Serial.println(ledState ? "ON" : "OFF");
+  }
+}
